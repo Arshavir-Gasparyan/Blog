cceac2edfa7b0c63b60a71b1cf5dd3f48932c19c
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-07-25 15:13:13 +0400

    feat(CreatePosts): add Inputs and Button Components into CreatePosts page using Material-Ui

--- src/Components/CreatePosts/CreatePosts.js
+++ src/Components/CreatePosts/CreatePosts.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Component } from "react";
 import { Link } from "react-router-dom";
-import Input from "../Input/Input";
-import TextField from "@material-ui/core/TextField";
+import { TextField, Button, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default class CreatePosts extends Component {
   constructor(props) {
@@ -34,21 +34,52 @@ export default class CreatePosts extends Component {
 
   render() {
     return (
-      <div>
-        <TextField
-          onChange={this.handleOnchangeTitle}
-          type="text"
-          value={this.state.title}
-          id="standard-basic"
-          label="Standard"
-        />
-        <TextField id="standard-basic" label="Standard" />
-
-        <button onClick={this.onSubmitHandler}>
-          <Link to="/">click</Link>
-        </button>
-        <h2>{this.state.title}</h2>
-      </div>
+      <Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <h2>Tell us your story</h2>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <TextField
+            onChange={this.handleOnchangeTitle}
+            type="text"
+            value={this.state.title}
+            id="standard-basic"
+            label="Title"
+          />
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <TextField id="standard-basic" label="Content" />
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            onClick={this.onSubmitHandler}
+            variant="outlined"
+            color="primary"
+          >
+            <Link to="/">click</Link>
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
